96910fb34731f2ff1f877abff90ded96d68946e6
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2025-01-03 20:11:21 +0530

    Day07

---
+++ HTML_CSS_JS/Day07/demo1.js
@@ -0,0 +1,19 @@
+// console.log("Hello World...");
+// console.log('welcome to JS Module')
+// console.log(`
+//         Hopes so U r enjoyed this Module...
+//     `);
+
+var a1,a2;
+var a_1,a$1;
+
+var a=10;
+
+console.log("value of a is "+a);
+console.log('value of a is '+a);
+console.log(`
+        value of a is ${a}
+    `);
+
+var str='sumit';
+console.log("Value of str is "+str);
