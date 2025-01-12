ae52ce526162dff0768ee2de514008d4f3602387
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2025-01-12 17:17:09 +0530

    Day16

---
+++ Angular/Day16/stringinterpolation/stringinterpolation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stringinterpolation',
+  templateUrl: './stringinterpolation.component.html',
+  styleUrls: ['./stringinterpolation.component.css']
+})
+export class StringinterpolationComponent{
+  myname:string="Sumit Raokhande";
+  imgUrl:string="../../assets/bg4.jpg";
+  num:number=4;
+
+}
