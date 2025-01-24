f989a76b4b5e83562f9e81b193bd6baa22f4ba70
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2025-01-24 20:34:00 +0530

    Day25

---
+++ Angular/Day25/http.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  baseUrl:string='https://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http:HttpClient) { }
+
+  getPost(){
+   return (this.http.get(this.baseUrl));
+  }
+
+  PostData(obj:any){
+   return (this.http.post(this.baseUrl,obj));
+  }
+
+  UpdateData(obj:any){
+   return (this.http.put(`${this.baseUrl}/${obj.id}`,obj));
+  }
+
+  deleteData(id:any){
+   return (this.http.delete(`${this.baseUrl}/${id}`));
+  }
+
+  
+}
