c7d698f77268f1d2c0a8f85233c534b3f0ea628a
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2025-01-28 17:07:50 +0530

    Day28

---
+++ Angular/Day28/demotdf.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demotdf',
+  templateUrl: './demotdf.component.html',
+  styleUrls: ['./demotdf.component.css']
+})
+export class DemotdfComponent {
+
+  EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+  + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+}
