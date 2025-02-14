866ce125aa022e16eab4b5844709fa249334df27
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2025-02-14 16:31:19 +0530

    Day37

---
+++ Angular/Day37/http.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  baseUrl:string="http://localhost:8080/api/";
+  userBaseUrl:string='http://localhost:8080/user/';
+
+  constructor(private http:HttpClient) { }
+
+  getProductData(){
+   return (this.http.get(`${this.baseUrl}get-all`));
+  }
+
+  sigupUser(obj:any){
+    return (this.http.post(`${this.userBaseUrl}register`,obj,{
+      responseType:'text'
+    }));
+  }
+
+  login(obj:any){
+   return (this.http.post(`${this.baseUrl}auth/login`,obj))
+  }
+
+  PostProductData(productObj:any,image:File){
+    const formData=new FormData();
+    formData.append("product",JSON.stringify(productObj));
+    formData.append("image",image);
+   
+   return this.http.post(`${this.baseUrl}create`,formData,{
+      responseType:"text"
+    });
+  }
+
+  deleteData(id:any){
+    return (this.http.delete(`${this.baseUrl}delete/${id}`,{
+      responseType:"text"
+    }));
+  }
+
+}
